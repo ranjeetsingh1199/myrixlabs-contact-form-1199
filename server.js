21eda6d6e079c10fb341115551bcea0db967962f
Ranjeet Singh <ranjeet.singh@myrixlabs.com>
2025-09-02 10:19:33 +0530

    fix: correct nodemailer.createTransport function name

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.post('/contact', async (req, res) => {
     }
 
     // Brevo SMTP transporter
-    const transporter = nodemailer.createTransporter({
+    const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: process.env.EMAIL_PORT,
       secure: false,
